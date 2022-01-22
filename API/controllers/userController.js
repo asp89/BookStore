f2a01a2e0b9dcb4eef8826d9d5c8c92bedea67dc
Anjaneya <anjaneya.singh94@gmail.com>
2022-01-22 22:08:47 +0530

    user signup post route

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -1,10 +1,19 @@
-const user = require("../models/user");
+const User = require("../models/user");
 const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customError");
+const cookieToken = require("../utils/cookieToken");
 
 exports.signup = BigPromise(async (req, res, next) => {
-  // TODO
-  res.status(200).json({
-    success: true,
-    message: "Acknowledged",
+  const { name, email, password } = req.body;
+
+  if (!email || !name || !password) {
+    return next(new CustomError("Name, email and password are required", 400));
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
   });
+  await cookieToken(user, res);
 });
--- API/index.js
+++ API/index.js
@@ -1,4 +1,9 @@
 const app = require("./app");
+const connectWithDb = require("./config/db");
+require("dotenv").config();
+
+// NOTE: Connect with database
+connectWithDb();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running at port: ${process.env.PORT}`);
