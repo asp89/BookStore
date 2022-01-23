54bfc181a313a250eaa28f373c750eb86880e563
Anjaneya <anjaneya.singh94@gmail.com>
2022-01-23 16:27:37 +0530

    added node mailer

---
+++ API/utils/emailHelper.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const mailHelper = async (option) => {
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    },
+  });
+
+  const message = {
+    from: process.env.PRODUCT_SUPPORT,
+    to: option.receiverEmail,
+    subject: option.subject,
+    text: option.message
+  };
+
+  // send mail with defined transport object
+  await transporter.sendMail(message);
+};
+
+module.exports = mailHelper;
