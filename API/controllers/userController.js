44860121bfcc7056d250f9210cba35db6ffc4952
Anjaneya <anjaneya.singh94@gmail.com>
2022-01-25 22:21:17 +0530

    user dashboard route

--- API/controllers/userController.js
+++ API/controllers/userController.js
@@ -125,3 +125,11 @@ exports.resetPassword = BigPromise(async (req, res, next) => {
   await user.save();
   await cookieToken(user, res);
 });
+
+exports.getLoggedInUserDetails = BigPromise(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
