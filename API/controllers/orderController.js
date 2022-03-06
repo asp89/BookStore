aab60c61a56cfc3ccc69bdf64cafb412eed7acd0
Anjaneya <anjaneya.singh94@gmail.com>
2022-03-06 18:23:55 +0530

    order routes for admin role

--- API/controllers/orderController.js
+++ API/controllers/orderController.js
@@ -2,6 +2,7 @@ const Order = require("../models/order");
 const Product = require("../models/product");
 
 const BigPromise = require("../middlewares/bigPromise");
+const CustomError = require("../utils/customError");
 
 exports.createOrder = BigPromise(async (req, res, next) => {
   const {
@@ -28,3 +29,78 @@ exports.createOrder = BigPromise(async (req, res, next) => {
     order,
   });
 });
+
+exports.getOrderById = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
+
+  if (!order) {
+    return next(new CustomError("please check order id", 401));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.getOrdersByUserId = BigPromise(async (req, res, next) => {
+  const order = await Order.find({ user: req.user._id });
+
+  if (!order) {
+    return next(new CustomError("Invalid User Id", 401));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.getAllOrdersByAdmin = BigPromise(async (req, res, next) => {
+  const orders = await Order.find();
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+exports.updateOrderByAdmin = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus.toLowercase() === "delivered") {
+    return next(new CustomError("Order is already delivered.", 401));
+  }
+
+  order.orderStatus = req.body.orderStatus;
+
+  order.orderItems.forEach(async (item) => {
+    await updateProductStock(item.product, item.quantity);
+  });
+  await order.save();
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+exports.deleteOrderByAdmin = BigPromise(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+  await order.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateProductStock(productId, quantity) {
+  const product = await Product.findById(productId);
+  product.stock = product.stock - quantity;
+  await product.save({
+    validateBeforeSave: false,
+  });
+}
--- API/routes/orderRoute.js
+++ API/routes/orderRoute.js
@@ -1,8 +1,27 @@
 const express = require("express");
-const { createOrder } = require("../controllers/orderController");
+const {
+  createOrder,
+  getOrderById,
+  getOrdersByUserId,
+  getAllOrdersByAdmin,
+  deleteOrderByAdmin,
+  updateOrderByAdmin,
+} = require("../controllers/orderController");
 const router = express.Router();
 const { isLoggedIn, customRole } = require("../middlewares/user");
+const roles = require("../utils/roles");
 
 router.route("/order/create").post(isLoggedIn, createOrder);
+router.route("/order/:id").get(isLoggedIn, getOrderById);
+router.route("/allpurchases").get(isLoggedIn, getOrdersByUserId);
+
+router
+  .route("/admin/orders")
+  .get(isLoggedIn, customRole(roles.ADMIN), getAllOrdersByAdmin);
+
+router
+  .route("/admin/orders/:id")
+  .put(isLoggedIn, customRole(roles.ADMIN), updateOrderByAdmin)
+  .delete(isLoggedIn, customRole(roles.ADMIN), deleteOrderByAdmin);
 
 module.exports = router;
